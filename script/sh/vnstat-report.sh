369cc16453583213b1f5fe5dfa3105b300a4602b
Shawn Jim <jinzhenxiao@apache.org>
2024-02-21 00:57:16 +0800

    unsafe handling.

--- script/sh/vnstat-report.sh
+++ script/sh/vnstat-report.sh
@@ -3,10 +3,6 @@
 # 情况上报
 # 使用docker exec执行vnstat命令，然后用awk处理输出，最后通过read命令赋值给变量
 IFS=";" read -r date rx tx total <<< $(docker exec vnstat vnstat -d 1 --oneline | awk -F";" '{
-    split($3, arr, " ");
-    if (arr[2] == "MiB") $3 = sprintf("%.2f GiB", arr[1] / 1024);
-    else if (arr[2] == "KiB") $3 = sprintf("%.2f GiB", arr[1] / (1024 * 1024));
-
     split($4, arr, " ");
     if (arr[2] == "MiB") $4 = sprintf("%.2f GiB", arr[1] / 1024);
     else if (arr[2] == "KiB") $4 = sprintf("%.2f GiB", arr[1] / (1024 * 1024));
@@ -14,6 +10,10 @@ IFS=";" read -r date rx tx total <<< $(docker exec vnstat vnstat -d 1 --oneline 
     split($5, arr, " ");
     if (arr[2] == "MiB") $5 = sprintf("%.2f GiB", arr[1] / 1024);
     else if (arr[2] == "KiB") $5 = sprintf("%.2f GiB", arr[1] / (1024 * 1024));
+
+    split($6, arr, " ");
+    if (arr[2] == "MiB") $6 = sprintf("%.2f GiB", arr[1] / 1024);
+    else if (arr[2] == "KiB") $6 = sprintf("%.2f GiB", arr[1] / (1024 * 1024));
 
     print $3";"$4";"$5";"$6
 }')
