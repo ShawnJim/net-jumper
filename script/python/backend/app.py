20fce0ae3544b722b7c807b23876dca5fc909d21
Shawn Jim <jinzhenxiao@apache.org>
2024-02-21 14:02:55 +0800

    update app.py

--- script/python/backend/app.py
+++ script/python/backend/app.py
@@ -107,8 +107,8 @@ def to_v2ray_txt():
     vmess_group = []
     # 添加自定义头部
     use_net_info = vnstat_db_manager.select_total_for_day(db_file)
-    use_total = gib_to_bits(use_net_info['total'])
-    threshold = gib_to_bits(node_db_manager.select_total_threshold(db_file)[0])
+    use_total = use_net_info['total']
+    threshold = node_db_manager.select_total_threshold(db_file)[0]
     vmess_group.append(f"STATUS=当日总流量: {use_total}Gib / {threshold}Gib | $节点无法使用则为当日流量耗尽$ ")
     for proxy in proxy_list:
         vmess_dict = {
